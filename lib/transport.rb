c22130bce24668d7b03c4e7c5ef014c67f76bc12
Thanh Dinh Khac <thanhdk@gmail.com>
2015-11-07 17:55:40 +0800

    Add force schema option

--- lib/transport.rb
+++ lib/transport.rb
@@ -17,7 +17,7 @@ class Transport
     @dest_dbconfig[:port] ||= 5432
   end
 
-  def copy_tables(src_tablenames)
+  def copy_tables(src_tablenames, options={})
     schema_name = src_tablenames.split('.')[0]
     table_glob = src_tablenames.split('.')[1]
 
@@ -25,24 +25,24 @@ class Transport
     tables = dest_conn.list_tables(schema_name)
     tables.each do |table|
       if File.fnmatch(table_glob, table)
-        copy_table("#{schema_name}.#{table}")
+        copy_table("#{schema_name}.#{table}", options)
       end
     end
   end
 
-  def copy_table(src_tablename, dest_tablename=nil, options=nil)
+  def copy_table(src_tablename, dest_tablename=nil, options={})
     dest_tablename ||= src_tablename
-    options ||= {
-      create_schema: true,
-      skip_indexes: false
-    }
+    options[:create_schema] = true if options[:create_schema].nil?
+    options[:skip_indexes] = false if options[:skipe_indexes].nil?
 
     Pgcp.logger.info "Start to copy from table #{src_tablename} to table #{dest_tablename}"
     src_table = QualifiedName.new(src_tablename)
     dest_table = QualifiedName.new(dest_tablename)
+    dest_table.schema_name = options[:force_schema] if not options[:force_schema].nil?
 
     src_conn = Postgres.new(@src_dbconfig)
     dest_conn = Postgres.new(@dest_dbconfig)
+
     dest_conn.exec "CREATE SCHEMA IF NOT EXISTS #{dest_table.schema_name};" if options[:create_schema]
 
     src_indexes = src_conn.get_indexes(src_table.schema_name, src_table.table_name)
@@ -54,12 +54,19 @@ class Transport
                                             src_table.table_name,
                                             temp_table.schema_name,
                                             temp_table.table_name)
-      dest_conn.exec(create_table_statement)
-      Pgcp.logger.info "Copying table data to temporary table. This could take a while..."
-      direct_copy(src_table.full_name, temp_table.full_name)
-      Pgcp.logger.info "Hotswapping to destination table #{dest_tablename}"
-      dest_conn.hotswap_table(dest_table.schema_name, temp_table.table_name, dest_table.table_name)
-      Pgcp.logger.info "Done copying table data."
+        begin
+          dest_conn.exec(create_table_statement)
+          Pgcp.logger.info "Copying table data to temporary table. This could take a while..."
+          direct_copy(src_table.full_name, temp_table.full_name)
+          Pgcp.logger.info "Hotswapping to destination table #{dest_tablename}"
+          dest_conn.hotswap_table(dest_table.schema_name, temp_table.table_name, dest_table.table_name)
+          Pgcp.logger.info "Done copying table data."
+        rescue Exception => e
+          Pgcp.logger.error(e.message)
+          return
+        ensure
+          dest_conn.drop_table(temp_table.schema_name, temp_table.table_name)
+        end
     else
       Pgcp.logger.info "Destination table does not exist, creating destination table."
       create_table_statement =
@@ -67,10 +74,10 @@ class Transport
                                             src_table.table_name,
                                             dest_table.schema_name,
                                             dest_table.table_name)
-      dest_conn.exec(create_table_statement)
-      Pgcp.logger.info "Copying table data to destination table. This could take a while..."
-      direct_copy(src_table.full_name, dest_table.full_name)
-      Pgcp.logger.info "Copying table data to destination table done."
+        dest_conn.exec(create_table_statement)
+        Pgcp.logger.info "Copying table data to destination table. This could take a while..."
+        direct_copy(src_table.full_name, dest_table.full_name)
+        Pgcp.logger.info "Copying table data to destination table done."
     end
 
     unless options[:skip_indexes]
@@ -78,6 +85,7 @@ class Transport
       dest_conn.create_indexes(dest_table.schema_name, dest_table.table_name, src_indexes)
       Pgcp.logger.info "Done copying table indexes."
     end
+
   end
 
   private
@@ -87,7 +95,7 @@ class Transport
     sql_out = sql_copy_to_stdout(src_tablename)
     command = transfer_command(@src_dbconfig, @dest_dbconfig, sql_in, sql_out)
     `#{command}`
-    exit_code = `echo $?`
+    exit_code = `echo $?`.to_i
     if exit_code != 0
       raise 'Failed to directly copy data'
     end
@@ -113,7 +121,7 @@ class Transport
         -h #{src_dbconfig[:host]}
         -p #{src_dbconfig[:port]}
         -c "#{sql_out}"
-        #{src_dbconfig[:dbname]}
+    #{src_dbconfig[:dbname]}
     }.gsub(/\n/, ' ')
     copy_from_command = %Q{
       env PGPASSWORD="#{dest_dbconfig[:password]}"
@@ -122,7 +130,7 @@ class Transport
         -h #{dest_dbconfig[:host]}
         -p #{dest_dbconfig[:port]}
         -c "#{sql_in}"
-        #{dest_dbconfig[:dbname]}
+    #{dest_dbconfig[:dbname]}
     }.gsub(/\n/, ' ')
 
     "#{copy_to_command} | #{copy_from_command}"
